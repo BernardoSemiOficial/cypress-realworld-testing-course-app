1eec95490b4c86c4b9314a2e23a9f4f665f2890f
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-08-22 15:24:54 -0300

    feat: criado primeiros casos de testes para a página home

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,17 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("deve ter o titulo Testing Next.js Applications with Cypress no elemento h1 da página home", () => {
+    cy.get('[data-test="hero-heading"]').contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  it("deve ter 3 itens 4 Courses, 25+ Lessons e Free and Open Source", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  })
+})
