f6028e24c8e847ac76d36de725aa9573beedf87f
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-08-22 18:58:01 -0300

    feat: criado novos testes na página home.

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,15 +3,35 @@ describe("Home Page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("deve ter o titulo Testing Next.js Applications with Cypress no elemento h1 da página home", () => {
-    cy.getByData("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
+  context("Hero section", () => {
+    it("deve ter o titulo Testing Next.js Applications with Cypress no elemento h1 da página home", () => {
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
+
+    it("deve ter 3 itens 4 Courses, 25+ Lessons e Free and Open Source", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("deve ter 3 itens 4 Courses, 25+ Lessons e Free and Open Source", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      // cy.getByData("course-0").within(() => {
+      //   cy.get("a[href='/testing-your-first-application']").click()
+      // })
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
